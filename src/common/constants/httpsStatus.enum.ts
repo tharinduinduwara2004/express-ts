9fc4d698e25f8fcdaf2023b1445f1790ff20fc3e
tharinduinduwara2004 <tinduwara427@gmail.com>
2025-08-27 13:53:23 +0530

    export enum https status

---
+++ src/common/constants/httpsStatus.enum.ts
@@ -0,0 +1,10 @@
+export enum HttpsStatus{
+    OK = 200,
+    CREATED = 201,
+    NO_CONTENT = 204,
+    BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
+    NO_FOUND = 404,
+    INTERNAL_SERVER_ERROR = 500
+
+}
